3b6fc09ada30a89e7df3e6990a00725ac1f97fc1
Todor Markov <todorator@gmail.com>
2017-10-06 22:53:09 +0300

    Can POST MapEntities

--- MapApi/rest.py
+++ MapApi/rest.py
@@ -1,9 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.serializers import CharField, FloatField, IntegerField, DateTimeField, Serializer
 from rest_framework.serializers import UUIDField
-from rest_framework_gis.serializers import GeoFeatureModelSerializer
+from rest_framework_gis.serializers import GeometryField
 import uuid
 
 from django.http import Http404
@@ -11,29 +12,33 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import MapApp
+import pprint
 
 class MapEntitySerializer(Serializer):
     """ A class to serialize locations as GeoJSON compatible data """
 
     entityType = CharField(max_length=20, help_text="Insert type here")
-    # lat = FloatField()
-    # lng = FloatField()
+    # geoLocationField = GeometryField(srid=4326)
+    # insert hacks 
+    internal_request = None
+    owner = None
 
     def create(self, validated_data):
         """
         Create and return a new `Snippet` instance, given the validated data.
         """
+                
+        validated_data['owner_id'] = self.owner.id
+        m_ent = MapApp.models.MapEntity(**validated_data)
+        m_ent.owner = self.owner
+
+        m_ent.setLocationField(self.internal_request._data['lat']\
+            ,self.internal_request._data['lng'])
+        m_ent.save()
+        # pprint.pprint(self.internal_request.__dict__)
+        pprint.pprint(m_ent.__dict__)
+        return m_ent
         # return MapApp.models.MapEntity.objects.create(**validated_data)
-        user = None
-        request = self.context.get("request")
-        if request and hasattr(request, "user"):
-            user = request.user
-        else:
-            raise Exception("123")
-
-
-        validated_data['owner_id'] = user.id
-        return MapApp.models.MapEntity.objects.create(**validated_data)
 
     def update(self,  instance, validated_data):
         """
@@ -69,17 +74,23 @@ class EventQuery(APIView):
     parser_classes = (JSONParser,)
     renderer_classes = (JSONRenderer,)
     serializer_classes = (MapEntitySerializer,)
+    authentication_classes = (TokenAuthentication,)
 
     def get(self, request, format=None):
         # snippets = Snippet.objects.all()
         # serializer = SnippetSerializer(snippets, many=True)
-        user = None
-        if request and hasattr(request, "user"):
-            user = request.user
-        return Response([str(user)])
+        
+        return Response([str(123)])
 
     def post(self, request, format=None):
         serializer = MapEntitySerializer(data=request.data)
+
+        user = None
+        if request and hasattr(request, "user"):
+            user = request.user
+        serializer.owner = user
+        serializer.internal_request = request
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
