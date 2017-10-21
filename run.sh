47e6b799d2eee3e074ef88542e0a1e38e94ee7e1
Todor Markov <todorator@gmail.com>
2017-10-21 18:39:08 +0000

    changed run cconf

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 # gunicorn --threads 10 -b 0.0.0.0:80 -b 0.0.0.0:443 MapEngine.wsgi
 # gunicorn --certfile=certs/selfsigned.crt --keyfile=certs/selfsigned.key --threads 10 -b 0.0.0.0:80 MapEngine.wsgi
-gunicorn --certfile=certs/selfsigned.crt --keyfile=certs/selfsigned.key --threads 10 -b 0.0.0.0:80 MapEngine.wsgi
+gunicorn --certfile=certs/selfsigned.crt --keyfile=certs/selfsigned.key --threads 10 -b 0.0.0.0:443 MapEngine.wsgi
